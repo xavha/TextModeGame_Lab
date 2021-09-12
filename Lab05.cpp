b904cb68fdc741a7ca0bb12621a54fdb89008538
xavha <86247818+xavha@users.noreply.github.com>
2021-09-13 01:28:37 +0700

    Update Lab05.cpp

--- Lab05.cpp
+++ Lab05.cpp
@@ -3,18 +3,6 @@
 #include<conio.h>
 using namespace std;
 
-#define screen_x 80
-#define screen_y 25
-HANDLE wHnd;
-SMALL_RECT windowSize = { 0,0,screen_x-1,screen_y-1};
-
-int setConsole(int x, int y)
-{
-	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
-	return 0;
-}
-
 void drawShip(int x, int y) {
 	COORD c = { x,y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
@@ -32,7 +20,6 @@ void gotoxy(int x,int y) {
 }
 */
 int main() {
-	setConsole(screen_x, screen_y);
 	char ch = ' ';
 	int x = 38, y = 20;
 	drawShip(x,y);
